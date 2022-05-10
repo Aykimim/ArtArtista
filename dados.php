6eae6680a4cf843505afd0cc386d167798557a5f
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-05-10 01:56:17 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -1,10 +1,27 @@
 <?php
+
+<?php
+    if (isset($_POST["acao"])){
+    echo "<script>alert('Formulário Foi enviado pelo método POST')</script>";
+    }
+?>
+
+
+
+
+
+
+
+
+
+
+
+
 // A variável global $_POST permite acessar os dados enviados com o método POST pelo nome
    // Para acessar os dados enviados com o método GET, você pode usar $_GET
-  $nome = htmlspecialchars($_POST['nome']);
-$email = htmlspecialchars($_POST['email']);
-$telefone = htmlspecialchars($_POST['telefone']);
-  $msg  = htmlspecialchars($_POST['msg']);
-
-  echo  $say, ' ', $to;
+ // $nome = htmlspecialchars($_POST['nome']);
+//$email = htmlspecialchars($_POST['email']);
+//$telefone = htmlspecialchars($_POST['telefone']);
+//  $msg  = htmlspecialchars($_POST['msg']);
+// echo  $say, ' ', $to;
 ?>
