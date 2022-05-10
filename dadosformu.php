7df7bc167f3cc7c0ff965aec8790b92b4f52b3ac
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-05-10 19:43:16 -0300

    Update dadosformu.php

--- dadosformu.php
+++ dadosformu.php
@@ -1,7 +1,49 @@
 <?php
 
-phpinfo();
 
+    if (isset($_POST["vai"])){
+        echo "<script>alert('Formulário Foi enviado pelo método POST')</script>";
+        $nome = $_POST["nome"];
+        $telefone = $_POST["telefone"];
+        $email = $_POST["email"];
+   
+        $mensagem = $_POST["mensagem"];
+        
+         echo "<p>Olá, ".$nome."</p>"; 
+        
+        echo "<p>Seu email é: ".$email."</p>"; 
+
+        echo "<p>Seu telefone é: ".$telefone."</p>"; 
+
+        echo "<p>Sua mensagem é:<br/>".$mensagem."</p>"; 
+
+
+
+   # alterar a variavel abaixo colocando o seu email
+
+$destinatario = "eykimim@hotmail.com.br";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$telefone = $_REQUEST['telefone'];
+$mensagem = $_REQUEST['mensagem'];
+
+
+ // monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO - TESTE COMPROVATIVO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $mensagem, $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("location:home.html");
 ?>
 
 
