0cc1e59261b4ffefdc3a1b82ac10c98d0be38e4a
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-05-10 01:44:53 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -1,8 +1,10 @@
 <?php
 // A variável global $_POST permite acessar os dados enviados com o método POST pelo nome
    // Para acessar os dados enviados com o método GET, você pode usar $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
+  $nome = htmlspecialchars($_POST['nome']);
+$email = htmlspecialchars($_POST['email']);
+$telefone = htmlspecialchars($_POST['telefone']);
+  $msg  = htmlspecialchars($_POST['msg']);
 
   echo  $say, ' ', $to;
 ?>
